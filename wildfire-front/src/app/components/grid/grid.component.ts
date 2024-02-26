fd094193a66b17e61685d54ac4a13fcd7b2bcbaf
bastienjacoud <bastien.jacoud@hotmail.fr>
2024-02-26 22:27:52 +0100

    Adding grid component

---
+++ wildfire-front/src/app/components/grid/grid.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {MatCard, MatCardMdImage} from "@angular/material/card";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
+import {Tree} from "../../models/tree";
+import {TreeStatus} from "../../models/enums/tree-status";
+
+@Component({
+  selector: 'app-grid',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardMdImage,
+    MatGridList,
+    MatGridTile,
+    NgForOf,
+    NgIf,
+    NgOptimizedImage
+  ],
+  templateUrl: './grid.component.html',
+  styleUrl: './grid.component.css'
+})
+export class GridComponent implements OnInit {
+  @Input() treeList!: Tree[];
+  @Input() nbColumns!: number;
+
+  protected readonly TreeStatus = TreeStatus;
+
+  ngOnInit(): void {
+
+  }
+}
