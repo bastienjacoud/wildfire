3d7abb103b48bcad999355ab4f4a7809d43b8aea
bastienjacoud <bastien.jacoud@hotmail.fr>
2024-02-26 22:26:01 +0100

    Updated app component

--- wildfire-front/src/app/app.component.ts
+++ wildfire-front/src/app/app.component.ts
@@ -1,17 +1,72 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatGridListModule} from "@angular/material/grid-list";
+import {Component, OnInit} from '@angular/core';
+import {TreeStatus} from "./models/enums/tree-status";
+import {GridComponent} from "./components/grid/grid.component";
+import {ActionsComponent} from "./components/actions/actions.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    MatGridListModule
+    GridComponent,
+    ActionsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'wildfire-front';
+  treeList = [
+    {
+      status : TreeStatus.NORMAL,
+      posX : 0,
+      posY : 0
+    },
+    {
+      status : TreeStatus.NORMAL,
+      posX : 1,
+      posY : 0
+    },
+    {
+      status : TreeStatus.NORMAL,
+      posX : 2,
+      posY : 0
+    },
+    {
+      status : TreeStatus.NORMAL,
+      posX : 0,
+      posY : 1
+    },
+    {
+      status : TreeStatus.NORMAL,
+      posX : 1,
+      posY : 1
+    },
+    {
+      status : TreeStatus.NORMAL,
+      posX : 2,
+      posY : 1
+    },
+    {
+      status : TreeStatus.NORMAL,
+      posX : 0,
+      posY : 2
+    },
+    {
+      status : TreeStatus.FIRE,
+      posX : 1,
+      posY : 2
+    },
+    {
+      status : TreeStatus.CINDER,
+      posX : 2,
+      posY : 2
+    }
+  ];
+  nbColumns=3;
+
+  ngOnInit(): void {
+
+  }
 }
